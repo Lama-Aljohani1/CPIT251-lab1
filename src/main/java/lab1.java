b9012ca5d28d2c9bc0f85d640f128b208a069db1
Lama Aljohani <lmohammedaljohani@stu.kau.edu.sa>
2023-09-03 13:09:36 +0300

    CPIT251-lab1

---
+++ src/main/java/lab1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author 96654
+ */
+public class lab1 {
+
+    public static void main(String[] args) {
+        System.out.print("Start CPIT-251 labs");
+        
+    }
+    
+}
